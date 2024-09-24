afdfe50662f043bb7c41d34fd146cc50d6e9f46d
Matteo <matteo.ciardi26@gmail.com>
2024-09-24 12:25:10 +0200

    esercizio finito

---
+++ Es_1_TassoDiIdratazione_Ciardi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasso di idratazione</title>
+</head>
+<body>
+
+    <h1 style="color : red;">Ciardi Matteo</h1>
+    <?php 
+       
+    $peso = 70;
+    $ML_KILO = 30;
+    $tassoId = $peso*$ML_KILO;   
+    echo "<p>Il tasso di idratazione di una persona che pesa $peso kg è di $tassoId</p>";
+
+    
+    ?>
+    
+</body>
+</html>
